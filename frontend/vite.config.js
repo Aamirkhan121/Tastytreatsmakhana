3996624ff34e0c139ad50afea53c3e9ad885959a
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-05-06 14:13:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,11 +11,6 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/api': {
         target: 'https://tastytreatsmakhana.onrender.com',
         changeOrigin: true,
         secure: false,
