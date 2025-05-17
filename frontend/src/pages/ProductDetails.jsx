957b618eb553b9ed358f1223aaa0a48fa1a02ac0
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-05-17 12:16:14 +0530

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 const navigate = useNavigate();
 
