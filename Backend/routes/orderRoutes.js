493948a6bdd50dd0567d3da8a02c4c6a584ab909
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-05-17 14:22:07 +0530

    Update orderRoutes.js

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { placeOrder, getAllOrders } from '../controllers/orderControllers.js';
+import { placeOrder } from '../controllers/orderControllers.js';
+// import { placeOrder, getAllOrders } from '../controllers/orderControllers.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/create', protect, placeOrder);
-router.get('/admin', protect, isAdmin, getAllOrders);
+// router.get('/admin', protect, isAdmin, getAllOrders);
 
 export default router;
