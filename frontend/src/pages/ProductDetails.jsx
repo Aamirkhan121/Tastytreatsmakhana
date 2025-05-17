ef38c4d4d0c37d908dbb6e76ad11091eb778f1e7
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-05-17 12:15:25 +0530

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+const navigate = useNavigate();
 
 const ProductDetails = () => {
   const { productId } = useParams();
