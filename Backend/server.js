214c2ad23a40abee34d8bb1d4b9972bd3ec93cbd
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-05-31 10:24:09 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -55,7 +55,7 @@ app.post("/api/payment/order", async(req,res)=>{
 
 res.status(200).json(order)
   } catch (error) {
-      console.error("Error in /api/payment/order route:", error);
+      console.error("Error", error);
      res.status(500).json({ message: "Internal server error", error: error.message });
     
   }
