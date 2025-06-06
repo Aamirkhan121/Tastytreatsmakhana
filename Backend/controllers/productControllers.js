399ecfdf23e2a3c4a2700241654da6fac1e1dc89
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-06-06 08:41:35 +0530

    Update productControllers.js

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -67,5 +67,21 @@ const deleteProduct = async (req, res) => {
   }
 };
 
+// const deleteProduct = async (req, res) => {
+//   try {
+//     const product = await Product.findById(req.params.id);
+
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not found" });
+//     }
+
+//     await product.deleteOne();
+//     res.status(200).json({ message: "Product deleted successfully" });
+//   } catch (error) {
+//     console.error("Delete Error:", error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
+
 export { getProducts, getProductById, createProduct, updateProduct, deleteProduct };
 // import Product from "../models/Product.js";
