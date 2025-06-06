c712239b4c6ff5f502e3107c14cae59106540fb3
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-06-06 08:40:08 +0530

    Update productRoutes.js

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -8,16 +8,7 @@ router.post('/',createProduct)
 router.put('/:id',updateProduct)
 router.get('/:id', getProductById); // Assuming you want to get a product by ID as well
 // Example Express route
-router.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-
-  await product.deleteOne();
-  res.status(200).json({ message: "Product deleted successfully" });
-});
+router.delete("/delete/:id");
 
 
 export default router;
