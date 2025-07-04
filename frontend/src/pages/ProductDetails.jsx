6b85ab2a7def957d1973b59ee0ae705f83f0a5d1
Aamirkhan121 <aamir73690@gmail.com>
2025-07-04 20:11:17 +0530

    productdetails update

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const ProductDetails = () => {
   const { productId } = useParams();
