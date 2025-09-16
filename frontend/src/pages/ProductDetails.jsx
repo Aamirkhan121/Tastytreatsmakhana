0bb6ceb63984dbf77fe582555490864eed9dbd53
Aamirkhan121 <131248243+Aamirkhan121@users.noreply.github.com>
2025-09-16 14:18:38 +0530

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -173,10 +173,21 @@ Black pepper
                 transition={{ delay: 0.4 }}
               >
                 <p className="text-3xl font-bold text-orange-600">₹{products.price}</p>
-              <p className="text-lg text-gray-700 font-medium">{products.description}</p>
-              <p className="text-lg text-gray-700 font-medium" style={{ whiteSpace: 'pre-line' }}>
-        {descriptionText}
-      </p>
+            <p className="text-lg text-gray-700 font-medium" style={{ whiteSpace: 'pre-line' }}>
+  {products.slug === "navratri-upwas-combo" 
+    ? `Celebrate this Navratri with guilt-free, crunchy, and delicious Tasty Crunch Roasted Makhana. Packed with flavor and nutrition.
+
+Our Combo Pack of 3 brings you the perfect festive snacking experience at a special price!
+
+Flavours
+Himalayan salted
+Creamy cheese
+Black pepper
+
+1 pack = 60 Grams`
+    : products.description // baaki products ka description backend se
+  }
+</p>
                 
 
                 {/* Rating */}
